192d67a1547fdc8536bb06f09310a373d3ed5d71
ryosuke-toyama <69573190+ryosuke-toyama@users.noreply.github.com>
2020-10-17 20:56:33 +0900

    ActionCable導入

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -7,9 +7,27 @@ class ProblemsController < ApplicationController
   end
 
   def create
+    @room = Room.find(params[:room_id])
+    @problems = @room.problems.all
+    @problem = @room.problems.new(problem_params)
+    respond_to do |format| 
+      if @problem.save 
+        ActionCable.server.broadcast 'room_channel', problem: @problem 
+        format.html { redirect_to room_problems_path(room) } 
+        format.json { render 'index.json', status: :created, location: @problem } 
+        format.js 
+      else 
+        format.html { render :index } 
+        format.json { render json: @problem.errors, status: :unprocessable_entity } 
+      end 
+    end 
   end
 
   def destroy
   end
 
+  private
+  def problem_params
+    params.require(:problem).permit(:content, :image).merge(user_id: current_user.id)
+  end
 end
---
+++ app/javascript/channels/room_channel.js
@@ -0,0 +1,31 @@
+import consumer from "./consumer"
+
+consumer.subscriptions.create("RoomChannel", {
+  connected: function() {
+    //called when the subscription is ready for use on the server
+  },
+
+  disconnected: function() {
+    // Called when the subscription has been terminated by the server
+  },
+
+  received: function(data) {
+    const problemContents = document.getElementById('problem-contents')
+    const pattern = /T\d{1,2}:\d{2}/;
+    const extraction = data.problem.created_at.match(pattern);
+    const strftime = extraction[0].slice(1);
+    const HTML = `  
+      <li class="problem-direction">
+        <div class="problem-text">
+          ${data.problem.content}
+        </div>
+        <div class="created_at">
+          ${strftime}
+        </div>
+      </li>`;
+    problemContents.insertAdjacentHTML("beforeend", HTML);
+
+    const formText = document.getElementById("todo");
+    formText.value = "";
+  }
+});
